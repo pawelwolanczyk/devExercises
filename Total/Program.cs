51ce2a25b86a67a9951eae0c295c83ce75d973aa
jkiedrowska85 <jkiedrowska85@gmail.com>
2022-10-06 08:20:42 +0200

    Napisz program który będzie sumował liczby. Po uruchomieniu program ma zapytać o pierwszą liczbę, a następnie pytać użytkownika o kolejną liczbę lub znak q aby zakończyć program. Jeśli użytkownik naciśnie q program ma wyświetlić sumę wszystkich podanych wcześniej liczb i zakończyć działanie.

---
+++ Total/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Total
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę: ");
+            double number1;
+            bool resParse1;
+            resParse1 = double.TryParse(Console.ReadLine(), out number1);            
+            if (resParse1 == true)
+            {
+                List<double> list = new List<double>();
+                double numbers;
+                for (int i = 0; i >= list.Count; i++)
+                {
+                    Console.WriteLine("Podaj kolejną liczbę: ");
+                    string s = Console.ReadLine();
+                    bool resParse2;
+                    resParse2 = double.TryParse(s, out numbers);
+                    if (resParse2 == true)
+                    {
+                        list.Add(numbers);
+                    }
+                    else if (s == "q")
+                    {
+                        double suma = number1;
+                        for (int j = 0; j < list.Count; j++)
+                        {
+                            suma += list[j];
+                        }
+                        Console.WriteLine("Suma: " + suma);
+                        //break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie rozumiem polecenia!");
+                        break;
+                    }
+                }
+            }
+            else
+            Console.WriteLine("Nie rozumiem polecenia!");
+        }
+    }
+}
+
+//for (int i = 0; i >= 0; i++)
+//{
+//    Console.WriteLine("Podaj kolejną liczbę: ");
+//    double number2;
+//    bool resParse2;
+//    //double wynik = number1;
+//    string[] s = new string[] {Console.ReadLine()};
+//    resParse2 = double.TryParse(args[], out number2);
+//    if (resParse2 == true)
+//    {
+//        number1 += number2;
+//        i++;
+//    }
+//    else
+//    {
+//        //char q = Convert.ToChar(Console.ReadLine());
+//        //if ()
+//        Console.WriteLine("Suma: " + number1);
+//    }
+//}
