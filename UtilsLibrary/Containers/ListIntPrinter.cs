80917d39357392d3498c9871b653a6386e1cac9a
pawel-wolanczyk-swi <pawel.wolanczyk@solarwinds.com>
2022-10-19 18:59:59 +0200

    Sorted list of ints

---
+++ UtilsLibrary/Containers/ListIntPrinter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsLibrary.Containers
+{
+    public class ListIntPrinter
+    {
+        public static void StaticPrint(SortedIntList list)
+        {
+            string result = String.Empty;
+            ListItem element = list.GetBegin();
+
+            while (element != null)
+            {
+                result += element.GetValue();
+                if (element._next != null)
+                    result += "\n";
+                element = element._next;
+            }
+
+            Console.Write(result);
+        }
+
+        public void ObjectPrint(SortedIntList list)
+        {
+            string result = String.Empty;
+            ListItem element = list.GetBegin();
+
+            while (element != null)
+            {
+                result += element.GetValue();
+                if (element._next != null)
+                    result += "\n";
+                element = element._next;
+            }
+
+            Console.Write(result);
+        }
+    }
+}
---
+++ UtilsLibrary/Containers/ListItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsLibrary.Containers
+{
+    internal class ListItem
+    {
+        private int _value;
+        public ListItem _next;
+
+        public ListItem(int a)
+        {
+            _value = a;
+            _next = null;
+        }
+
+        public int GetValue()
+        {
+            return _value;
+        }
+    }
+}
