d41cec350c7828a46a6ccc42f7d43eac396ac789
jkiedrowska85 <jkiedrowska85@gmail.com>
2022-11-09 12:35:55 +0100

    Changed structurer

--- database/System_Rezerwacji/Create_Reservation_System.sql
+++ database/System_Rezerwacji/Create_Reservation_System.sql
@@ -5,6 +5,13 @@ Zajêcia (klucz obcy powiazny z lokalizacj¹, drugi klucz z Terminami )
 Terminy --do przemyœlenia
 Kalendarz do rezerwacji 
 
+CREATE TABLE Levels(
+LevelID INT IDENTITY(1,1) NOT NULL,
+Level_Option NVARCHAR(12) NOT NULL, --pocz¹tkuj¹cy, œredni, œrednio zaawansowany, zaawansowany
+Level_Description NVARCHAR(200) NOT NULL, 
+CONSTRAINT pk_Levels_LevelID PRIMARY KEY (LevelID),
+CONSTRAINT uk_Levels_Level_Option UNIQUE (Level_Option)
+)
 
 CREATE TABLE Useres(
 UserID int IDENTITY(1,1) NOT NULL,
@@ -18,35 +25,12 @@ CONSTRAINT pk_User_UserID PRIMARY KEY (UserID),
 CONSTRAINT fk_Users_User_Level FOREIGN KEY (User_Level) REFERENCES Levels(LevelID) 
 )
 
-
-CREATE TABLE Levels(
-LevelID INT IDENTITY(1,1) NOT NULL,
-Level_Option NVARCHAR(12) NOT NULL, --pocz¹tkuj¹cy, œredni, œrednio zaawansowany, zaawansowany
-Level_Description NVARCHAR(200) NOT NULL, 
-CONSTRAINT pk_Levels_LevelID PRIMARY KEY (LevelID),
-CONSTRAINT uk_Levels_Level_Option UNIQUE (Level_Option)
+CREATE TABLE Times(
+TimeId int IDENTITY(1,1) NOT NULL,
+Day_Time Time NOT NULL,
+CONSTRAINT pk_Times_TimeID PRIMARY KEY (TimeID),
+CONSTRAINT uk_Times_Day_Time UNIQUE (Day_Time),
 )
-
-
-CREATE TABLE Carnets_Types(
-Carnet_TypeID INT IDENTITY(1,1) NOT NULL,
-Carnet_Type NVARCHAR(5), --CZYLI MIESIÊCZNY, RAZ W TYG; MIESIÊCZNY DWA RAZY W TYG ITD.ITP. jednorazowe wejœcie KLUCZ OBCY Z TABEL¥ Carnet
-Carnet_Price FLOAT NOT NULL,
-Description_Carnet NVARCHAR(300) NOT NULL, --opisujê ile treningów i ¿e w opcjach karnetu mogê wybraæ miejsce gdzie chce chodziæ na trening
-Option_Carnet INT NOT NULL, --TU MA MIEÆ WYPISANY ODNOŒNIK DO KA¯DEGO DNIA, PONIEWA¯ W ZALE¯NOŒCI OD WYBORU KARNETU (JEDEN, DWA, TRZY RAZY W TYGODNIU TO TAKIE MAM OGRANICZENIE WYBORU). I NP. JAK KLIKNÊ W JEDYNKÊ TO MAM PONIEDZIA£EK GDZIE MAM WYBÓR DO TRENINGU W DWÓCH RÓ¯NYCH GODZINACH W DWÓCH RÓ¯NYCH LOKALIZACJACH
-CONSTRAINT pk_Carnets_Types_Carnet_TypeID PRIMARY KEY (Carnet_TypeID),
-CONSTRAINT fk_Carnets_CarnetID FOREIGN KEY (Option_Carnet) REFERENCES Carnets(CarnetID)
-)
-
-
-CREATE TABLE Carnets( --Wybieramy dni tygodnia oraz godziny -> godziny musz¹ byæ powi¹zane z tabel¹ lokalizacji bo godzina treningu jest powi¹zana z miejsem treningu 
-CarnetID int IDENTITY(1,1) NOT NULL,
-Day NVARCHAR(9),-- Podajê dni tygodnia i te dni tygodnia s¹ powi¹zane z godzin¹ i lokalizacj¹ Naje¿dzam na dzieñ i wyœwietlaj¹ mi siê lokalizacjê gdzie odbywaj¹ siê treningi. Klikam w okalizacjê i tam wyœwietlaj¹ mi siê godziny w jakich odbywaj¹ siê treningi.
-Carnet_Location int NOT NULL, -- Tutaj klucz obcy
-CONSTRAINT pk_Carnet_CarnetID PRIMARY KEY (CarnetID),
-CONSTRAINT fk_Carnet_Carnet_Location FOREIGN KEY (Carnet_Location) REFERENCES Locations(LocationID)
-)
-
 
 CREATE TABLE Locations(
 LocationID int IDENTITY (1,1), 
@@ -56,12 +40,22 @@ CONSTRAINT pk_Locations_LocationID PRIMARY KEY (LocationID),
 CONSTRAINT fk_Locations_Location_Time FOREIGN KEY (Location_Time) REFERENCES Times(TimeId)
 )
 
+CREATE TABLE Carnets( --Wybieramy dni tygodnia oraz godziny -> godziny musz¹ byæ powi¹zane z tabel¹ lokalizacji bo godzina treningu jest powi¹zana z miejsem treningu 
+CarnetID int IDENTITY(1,1) NOT NULL,
+Day NVARCHAR(9),-- Podajê dni tygodnia i te dni tygodnia s¹ powi¹zane z godzin¹ i lokalizacj¹ Naje¿dzam na dzieñ i wyœwietlaj¹ mi siê lokalizacjê gdzie odbywaj¹ siê treningi. Klikam w okalizacjê i tam wyœwietlaj¹ mi siê godziny w jakich odbywaj¹ siê treningi.
+Carnet_Location int NOT NULL, -- Tutaj klucz obcy
+CONSTRAINT pk_Carnets_CarnetID PRIMARY KEY (CarnetID),
+CONSTRAINT fk_Carnets_Carnet_Location FOREIGN KEY (Carnet_Location) REFERENCES Locations(LocationID)
+)
 
-CREATE TABLE Times(
-TimeId int IDENTITY(1,1) NOT NULL,
-Day_Time Time NOT NULL,
-CONSTRAINT pk_Times_TimeID PRIMARY KEY (TimeID),
-CONSTRAINT uk_Times_Day_Time UNIQUE (Day_Time),
+CREATE TABLE Carnets_Types(
+Carnet_TypeID INT IDENTITY(1,1) NOT NULL,
+Carnet_Type NVARCHAR(5), --CZYLI MIESIÊCZNY, RAZ W TYG; MIESIÊCZNY DWA RAZY W TYG ITD.ITP. jednorazowe wejœcie KLUCZ OBCY Z TABEL¥ Carnet
+Carnet_Price FLOAT NOT NULL,
+Description_Carnet NVARCHAR(300) NOT NULL, --opisujê ile treningów i ¿e w opcjach karnetu mogê wybraæ miejsce gdzie chce chodziæ na trening
+Option_Carnet INT NOT NULL, --TU MA MIEÆ WYPISANY ODNOŒNIK DO KA¯DEGO DNIA, PONIEWA¯ W ZALE¯NOŒCI OD WYBORU KARNETU (JEDEN, DWA, TRZY RAZY W TYGODNIU TO TAKIE MAM OGRANICZENIE WYBORU). I NP. JAK KLIKNÊ W JEDYNKÊ TO MAM PONIEDZIA£EK GDZIE MAM WYBÓR DO TRENINGU W DWÓCH RÓ¯NYCH GODZINACH W DWÓCH RÓ¯NYCH LOKALIZACJACH
+CONSTRAINT pk_Carnets_Types_Carnet_TypeID PRIMARY KEY (Carnet_TypeID),
+CONSTRAINT fk_Carnets_CarnetID FOREIGN KEY (Option_Carnet) REFERENCES Carnets(CarnetID)
 )
 
 
