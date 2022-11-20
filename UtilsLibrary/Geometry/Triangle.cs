068caf0954239aeef00a6a82d1c092580198eb92
pawel-wolanczyk-swi <pawel.wolanczyk@solarwinds.com>
2022-11-20 22:35:03 +0100

    Workshop 20.11.2022 - C# abstract classes and methods, inheritance, interfaces

---
+++ UtilsLibrary/Geometry/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsLibrary.Geometry
+{
+    public class TriangleEquailteral : Shape
+    {
+        private int _podstawa;
+        private int _wysokosc;
+
+        public void Podstawa(int a)
+        {
+            _podstawa = a;
+        }
+
+        public void Wysokosc(int b)
+        {
+            _wysokosc = b;
+        }
+
+        public override double Area()
+        {
+            return _podstawa * _wysokosc * 0.5;
+        }
+
+        public override double Perimeter()
+        {
+            return 3 * _podstawa; 
+        }
+
+        public override string ToString()
+        {
+            return "Jestem trójkątem równobocznym";
+        }
+    }
+}
