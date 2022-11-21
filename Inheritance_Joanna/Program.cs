2eab4d1f2d06166a7e34fb67fe159ddd2296abca
jkiedrowska85 <jkiedrowska85@gmail.com>
2022-11-21 12:48:47 +0100

    A task with inheritance

---
+++ Inheritance_Joanna/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilsLibrary.Inheritance_Car;
+
+namespace Inheritance_Joanna
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                ArrayList cars = new ArrayList();
+                cars.Add(new Skoda());
+                cars.Add(new Mazda());
+            }
+        }
+    }
+}
