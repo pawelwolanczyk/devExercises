43ab7cb4aae68f6d6c6a1182f0c9ff7ccfd1a4b1
jkiedrowska85 <jkiedrowska85@gmail.com>
2022-11-28 14:06:10 +0100

    I removed unnecessary 'SELECT'

--- database/homework_Joanna/Sql_Homework_8.11.sql
+++ database/homework_Joanna/Sql_Homework_8.11.sql
@@ -1,19 +1,3 @@
---(Z Bats) Za pomoc¹ where i INNER JOIN wyci¹gnijcie informacje o desce która jest u¿ywana w danym zestawie
-SELECT * FROM Bats, Blades
-WHERE (Bats.BladeID = Blades.BladeID)
-
-SELECT * FROM Bats, Blades
-INNER JOIN Bats ON Bats.BladeID = Blades.BladeID
-
-INNER JOIN Rubbers ON Bats.Rubber1ID = Rubbers.RubberID AND Bats.Rubber2ID = Rubbers.RubberID
-WHERE Bats.BatID = 5
-
-SELECT * FROM Blades
-INNER JOIN Bats ON Blades.BladeID = Bats.BladeID
-
-SELECT * FROM Bats, Blades
-WHERE (Bats.BladeID = Blades.BladeID)
-
 SELECT * FROM Bats, Rubbers 
 WHERE (Bats.Rubber1ID = Rubbers.RubberID OR Bats.Rubber2ID = Rubbers.RubberID) 
 AND Bats.BatID = 5
