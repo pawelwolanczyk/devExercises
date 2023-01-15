f4e140c4f15b3c70c11a25862d4fff6b50c57094
jkiedrowska85 <jkiedrowska85@gmail.com>
2023-01-15 14:13:48 +0100

    New unit test in VideoRentalUnitTest MoviePsychicalTest numberOfAllPhisicalFilms_True

---
+++ VideoRentalUnitTests/MoviePhysicalTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoRental.Movies;
+
+namespace VideoRentalUnitTests
+{
+    [TestFixture]
+    public class MoviePhysicalTest
+    {
+        [Test]
+        public void MoviePsychical__numberOfAllPhisicalFilms_True()
+        {
+            //given //when
+            BluRayMovie brmovie = new BluRayMovie("Garfild", 2);
+            DvdMovie dvdMovie = new DvdMovie("Za szybko za wściekle", 3);
+
+            //than
+            Assert.True(MoviePhisical.CalculateAllFilms_Static() == 5);
+        }
+    }
+}
