982c0af9fda6395818fd035f5bf892557590ddad
jkiedrowska85 <jkiedrowska85@gmail.com>
2023-03-10 12:50:10 +0100

    Test with moq in VideoRentalUnitTest in RentalManagmentRentalAddMovieTest

---
+++ VideoRentalUnitTests/RentalManagmentRentalAddMovieTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using VideoRental.Movies;
+using VideoRental.RentalManagement;
+using VideoRental;
+
+namespace VideoRentalUnitTests
+{
+    [TestFixture]
+    internal class RentalManagmentRentalAddMovieTest
+    {
+        [Test]
+        public void AddMovieToList_allMovies()
+        {
+            //given
+            BluRayMovie movie = new BluRayMovie("Garfild", 2);
+            Mock<IMovie> mock = new Mock<IMovie>();
+            mock.Setup(i => i.Title).Returns(movie.Title);
+            mock.Setup(i => i.IsAvailable).Returns(true);
+            mock.Setup(i => i.Rent(DateTime.Now, DateTime.Now.AddDays(1)));
+
+            //when
+            var authentication = new Rental();
+            RentalMovies _allMovies = new RentalMovies();
+            IMovie movieTitle = mock.Object;
+            authentication.AddMovie(movieTitle);
+
+            //then 
+            Assert.True(_allMovies[0] == movieTitle);
+        }
+    }
+}
