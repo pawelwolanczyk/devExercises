1d3e3e7224b3826170176584d2c2450eccfe0b97
pawel-wolanczyk-swi <pawel.wolanczyk@solarwinds.com>
2023-03-30 02:39:05 +0200

    Partial class and extension methods

---
+++ ExtensionsAndPartial/Extensions/VhsRewindExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoRental.Movies;
+
+namespace ExtensionsAndPartial.Extensions
+{
+    public static class VhsRewindExtension
+    {
+        public static bool Rewind(this VhsMovie vhsMovie)
+        {
+            return true;
+        }
+
+        public static void AlaMaKota(this IMovie movie)
+        {
+            Console.WriteLine("Rozszerzenie IMovie");
+        }
+
+        public static int CompareTo(this MoviePhisical movie, int obj)
+        {
+            return 0;
+        }
+    }
+}
---
+++ ExtensionsAndPartial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoRental.Movies;
+using ExtensionsAndPartial.Extensions;
+using ExtensionsAndPartial.PartialClasses;
+
+namespace ExtensionsAndPartial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VhsMovie vhsMovie = new VhsMovie("Krzyżacy", 2);
+            vhsMovie.Rewind();
+            vhsMovie.AlaMaKota();
+            vhsMovie.CompareTo(1);  //in this case original (class) implementation will be executed (not extension method)
+
+            PartialClass pC = new PartialClass();
+            pC.DoSth();
+            pC.DoSthMore();
+        }
+    }
+}
